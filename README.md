6b2da8d105e6a5a69d82282b86399fd0d166ffef
RNogales94 <rafael.nogales@shell.com>
2023-06-10 17:51:49 +0200

    primera version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # practica1-ciberseguridad-2023
 Ejercicio de practicas 1
+
+kjlkj
---
+++ api.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from query import get_company_information
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/api/yahoo')
+def get_company():
+    symbol = "DIS"
+    return get_company_information(symbol)
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
